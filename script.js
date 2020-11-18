0eee35e16d61b922e85e2dea93a9886ecc2b5e45
jdavidrice <jdavidrice@gmail.com>
2020-11-18 13:25:08 -0500

    Instructions, JS plan

--- script.js
+++ script.js
@@ -10,3 +10,20 @@
 // WHEN the game is over
 // THEN I can save my initials and score
 
+// THINGS TO MAKE
+// 1. Text display area where questions appear.
+// 2. Answer choices for the questions.
+// 3. Button to start the quiz.
+// 4. Button to submit answer and move to next question.
+// 5. Timer that count downs a set amount of time to complete the quiz.
+// 6. Title of game with simple instructions. 
+// 7. List of questions and answers that are in an array and are cycled through.
+// 8. Score display zone - after the game is done, player can enter in inititals next to their score (in a table, along with date and time of score).
+
+// THINGS TO DO
+// 1. Event listener that indicates when the "start" button is clicked.
+// 2. Event listener that knows when correct or incorrect answer has been chosen and submitted, and diplays the result.
+// 3. Event listener that removes time from the clock (5 seconds?) for each incorrect answer.
+// 4. Function that advances to next question after the prior one is submitted.
+// 5. Function that tracks and displays score throughout the game.
+// 6. Event listener that knows when the last question is reached, or the timer reaches zero, and displays the final score with an option for the player to enter in their initials. 
